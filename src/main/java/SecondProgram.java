010eb5be4045fad5592f582b3ecac350466798fa
Lboy2006 <lboy2006@ya.ru>
2021-11-10 04:07:18 +0300

    Урок2

---
+++ src/main/java/SecondProgram.java
@@ -0,0 +1,15 @@
+public class SecondProgram {
+    public static void main(String[] args) {
+        System.out.println("Пивет, мир!!");
+        System.out.println(summa(21,6));
+        System.out.println(multipl(3,5));
+            }
+    public static int summa(int arg1, int arg2) {
+        int result = arg1 + arg2;
+        return result;
+    }
+    public static int multipl(int arg1, int arg2) {
+        return arg1 * arg2;
+         }
+}
+
