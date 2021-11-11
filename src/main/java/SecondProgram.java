39a0c31fbf9a0559be2ec6497190382228d0b290
Lboy2006 <lboy2006@ya.ru>
2021-11-11 15:50:14 +0300

    Урок2 исправленный

--- src/main/java/SecondProgram.java
+++ src/main/java/SecondProgram.java
@@ -1,15 +1,27 @@
 public class SecondProgram {
     public static void main(String[] args) {
         System.out.println("Пивет, мир!!");
-        System.out.println(summa(21,6));
-        System.out.println(multipl(3,5));
-            }
+        System.out.println(summa(21, 6));
+        System.out.println(multipl(3, 5));
+        System.out.println(dif(8, 4));
+        System.out.println(div(10, 5));
+    }
+
     public static int summa(int arg1, int arg2) {
         int result = arg1 + arg2;
         return result;
     }
+
     public static int multipl(int arg1, int arg2) {
         return arg1 * arg2;
-         }
+    }
+
+    public static int dif(int arg1, int arg2) {
+        return arg1 - arg2;
+    }
+
+    public static int div(int arg1, int arg2) {
+        return arg1 / arg2;
+    }
 }
 
