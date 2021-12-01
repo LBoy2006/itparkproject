7cc8c97a7617dc36bc9775e2a235a9339bf6dc77
Lboy2006 <lboy2006@ya.ru>
2021-12-01 21:48:11 +0300

    Урок 6 исправленный

--- src/main/java/lesson6/Lesson6.java
+++ src/main/java/lesson6/Lesson6.java
@@ -16,7 +16,28 @@ public class Lesson6 {
         System.out.println("Минимальное значение " + min);
         double avg = getAvg(massiv);
         System.out.println("Среднее значение " + avg);
+    }
 
+    public static int dlinaMassiva() {
+        Scanner scanner = new Scanner(System.in);
+        while (!scanner.hasNextInt()) {
+            System.out.println("Введите целое число:");
+            scanner.next();
+        }
+        int dlina = scanner.nextInt();
+        if (dlina < 0) {
+            System.out.println("Длина массива не может быть отрицательной, введите положительное число: ");
+            return dlinaMassiva();
+        }
+        return dlina;
+    }
+
+    private static int[] getMassiv(int dlina) {
+        int[] massive = new int[dlina];
+        for (int i = 0; i < dlina; i++) {
+            massive[i] = Math.round((float) Math.random() * 1000);
+        }
+        return massive;
     }
 
     private static int getMax(int[] massiv) {
@@ -43,29 +64,8 @@ public class Lesson6 {
         double avg = 0;
         for (int znachenie : massiv) {
             avg += znachenie;
-            }
+        }
         avg = avg / massiv.length;
         return avg;
-    }
-    private static int[] getMassiv(int dlina) {
-        int[] massive = new int[dlina];
-        for (int i = 0; i < dlina; i++) {
-            massive[i] = Math.round((float) Math.random() * 1000);
-        }
-        return massive;
-    }
-
-    public static int dlinaMassiva() {
-        Scanner scanner = new Scanner(System.in);
-        while (!scanner.hasNextInt()) {
-            System.out.println("Введите целое число:");
-            scanner.next();
-        }
-        int dlina = scanner.nextInt();
-        if (dlina < 0) {
-            System.out.println("Длина массива не может быть отрицательной, введите положительное число: ");
-            return dlinaMassiva();
-        }
-        return dlina;
     }
 }
